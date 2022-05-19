8c58a8acf1a750a6417c59af8590f26d3e8aa05d
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-05-19 21:02:48 +0530

    Minimal setup.py and Pytest based test infra
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ py-docgen:
 	python3 python/docgen.py > python/README.md
 
 docgen: py-docgen
+
+test-python:
+	pip3 install -e python/
+	pytest python/tests/
---
+++ python/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kannada",
+    description="Kannada language utilities",
+    packages=find_packages()
+)
